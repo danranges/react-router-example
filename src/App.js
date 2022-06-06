e6af71e54ddc8bc018aeb71793656e533d4c49a4
Dan Ranges <danranges@gmail.com>
2022-06-06 13:24:43 +0200

    Add invoice rendering, filtering, and deletion

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='App'>
+    <div style={{ paddingLeft: '1rem' }} className='App'>
       <h1>Bookkeeper!</h1>
       <nav style={{ borderBottom: 'solid 1px', paddingBottom: '1rem' }}>
         <Link to={'/invoices'}>Invoices</Link> |{' '}
         <Link to={'/expenses'}>Expenses</Link>
       </nav>
+      <Outlet />
     </div>
   );
 };
--- src/routes/Invoices.js
+++ src/routes/Invoices.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
+import QueryNavLink from './components/QueryNavLink';
+import { getInvoices } from '../data';
 
 const Invoices = () => {
+  let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+
   return (
-    <main style={{ padding: '1rem 0' }}>
-      <h2>Invoices</h2>
-    </main>
+    <div style={{ display: 'flex' }}>
+      <nav style={{ borderRight: 'solid 1px', padding: '1rem' }}>
+        <input
+          value={searchParams.get('filter') || ''}
+          onChange={(event) => {
+            let filter = event.target.value;
+            filter ? setSearchParams({ filter }) : setSearchParams({});
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter');
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <QueryNavLink
+              style={({ isActive }) => {
+                return {
+                  display: 'block',
+                  margin: '1rem 0',
+                  color: isActive ? 'red' : '',
+                };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </QueryNavLink>
+          ))}
+      </nav>
+      <Outlet />
+    </div>
   );
 };
 
